cc67ecac0154d9d47f295dcaf5b0553336b9a456
takashi ooizumi <ooizumit1980@gmail.com>
2023-02-13 13:21:13 +0900

    Identificationsモデルの作成

---
+++ db/migrate/20230213041622_create_identifications.rb
@@ -0,0 +1,17 @@
+class CreateIdentifications < ActiveRecord::Migration[7.0]
+  def change
+    create_table :identifications do |t|
+      t.integer :user_id, null:false
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.string :adress
+      t.string :phone_number
+      t.string :work
+      t.string :identification_data_path
+      t.boolean :confirm_flag
+      
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_034820) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_041622) do
+  create_table "identifications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.string "adress"
+    t.string "phone_number"
+    t.string "work"
+    t.string "identification_data_path"
+    t.boolean "confirm_flag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "avatar_name", null: false
     t.string "avatar_image_url", null: false
