d24ad0de7669c970fccbdcef5ed7cc8f22a75fff
takashi ooizumi <ooizumit1980@gmail.com>
2023-02-13 13:54:56 +0900

    areaモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_045118) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_045342) do
+  create_table "areas", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "address", null: false
+    t.string "address_code", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "block_members", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "target_user_id", null: false
