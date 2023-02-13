4978a4124d01ec64314d04e094bd6d64379326d2
takashi ooizumi <ooizumit1980@gmail.com>
2023-02-13 15:25:53 +0900

    モデル側のアソシエーションを作成

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ApplicationRecord
+  belongs_to :user, class_name: 'User', :foreign_key => 'user_id'
+  belongs_to :target_user, class_name: 'User', :foreign_key => 'target_user_id'
 end
--- app/models/playground.rb
+++ app/models/playground.rb
@@ -1,2 +1,4 @@
 class Playground < ApplicationRecord
+  belongs_to :area
+  belongs_to :playng_type
 end
